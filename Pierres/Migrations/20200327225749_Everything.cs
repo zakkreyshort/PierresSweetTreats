78aaf57aac3b8cbed231b7c7fc891bee7d19dcef
Zakkrey Short <zakkreyshort@Zakkreys-MacBook-Pro.local>
2020-03-27 16:07:02 -0700

    change context, and refactor view for index

---
+++ Pierres/Migrations/20200327225749_Everything.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pierres.Migrations
+{
+    public partial class Everything : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Flavors");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Flavors",
+                nullable: true);
+        }
+    }
+}
